2590a072077cc547fca0737c5a270b9375ca024f
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2023-01-19 16:17:59 -0300

    feat: consumindo API com json-server

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IReceitas } from 'src/app/interfaces/receitas';
+import { ReceitasService } from 'src/app/services/receitas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private receitasService: ReceitasService) { }
+
+  ngOnInit(): void {
+    this.getReceitas()
+  }
+
+  getReceitas(): void {
+    this.receitasService.getReceitas()
+      .subscribe((response: Array<IReceitas>) => {
+        console.log(response)
+      })
+  }
 }
