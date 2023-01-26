cf66e00f8b0e0371d9b564ce92008e38609533fa
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2023-01-25 21:20:41 -0300

    feat: adicionando imagens dinamica

--- src/app/views/receita/receita.component.ts
+++ src/app/views/receita/receita.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FirstImage } from 'src/app/enums/first-image';
+import { SecondImage } from 'src/app/enums/second-image';
 import { IReceitas } from 'src/app/interfaces/receitas';
 import { ReceitasService } from 'src/app/services/receitas.service';
 
@@ -11,6 +13,9 @@ import { ReceitasService } from 'src/app/services/receitas.service';
 export class ReceitaComponent implements OnInit {
 
   state: any;
+  receita!: IReceitas;
+  firstImage: string = ""
+  secondImage: string = ""
 
   constructor(
     private router: Router,
@@ -25,7 +30,28 @@ export class ReceitaComponent implements OnInit {
       this.receitasService.getReceita(this.state.id)
         .subscribe((response: IReceitas) => {
 
-          console.log(response)
+          this.receita = response;
+          
+          switch (this.receita.icone) {
+            case "receita01":
+              this.firstImage = FirstImage.receita01
+              this.secondImage = SecondImage.receita01
+              break;
+            case "receita02":
+              this.firstImage = FirstImage.receita02
+              this.secondImage = SecondImage.receita02
+              break;
+            case "receita03":
+              this.firstImage = FirstImage.receita03
+              this.secondImage = SecondImage.receita03
+              break;
+            case "receita04":
+              this.firstImage = FirstImage.receita04
+              this.secondImage = SecondImage.receita04
+              break;
+            default:
+              break;
+          }
         })
-  }
+    }
 }
